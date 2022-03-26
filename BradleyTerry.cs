c1f65e7af85be0e42d81b1aacbce4ba0b4f4d600
02Naitsirk <iifrostii@outlook.com>
2022-03-26 19:53:37 -0400

    MCC 20 update

--- BradleyTerry.cs
+++ BradleyTerry.cs
@@ -7,7 +7,7 @@ namespace MccSkill
     public static class BradleyTerry
     {
         // Number of MCCs.
-        public const int MccCount = 22;
+        public const int MccCount = 23;
 
         // List of players that have played in an MCC.
         public static readonly List<Player> PlayerList = new();
